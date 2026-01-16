0087cf3f7028020ab84f4e8d1705e700a6696bae
Taniya Bansal <taniya1agarwal@gmail.com>
2026-01-16 17:01:14 +0530

    fix: Correct import case for SignInPage and SignUpPage components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import SignInPage from './pages/SigninPage';
-import SignUpPage from './pages/SignupPage';
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
 import JoinUsPage from './pages/JoinUsPage';
 
 
